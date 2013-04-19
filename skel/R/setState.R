d65799874ace73cc1b9382d53a67362b2b03e0b3
surmann <surmann@surmann.(none)>
2013-04-19 15:30:30 +0200

    corrected example

--- skel/R/setState.R
+++ skel/R/setState.R
@@ -17,6 +17,10 @@
 #' @return an extended list of class [\code{\link{ODEnetwork}}].
 #' @exportMethod
 #' @examples
+#' masses <- 4:6
+#' dampers <- diag(1:3)
+#' springs <- diag(7:9)
+#' odenetwork <- ODEnetwork(masses, dampers, springs)
 #' position <- rep(10, 3)
 #' velocity <- rep(0, 3)
 #' odenetwork <- setState(odenetwork, position, velocity)
