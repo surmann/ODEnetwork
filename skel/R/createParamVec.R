db850d937e42f6b1324bbd3ea2a39dccdeea0bfc
surmann <surmann@surmann.(none)>
2013-04-19 16:07:04 +0200

    new: creates parameter vector of the given network

---
+++ skel/R/createParamVec.R
@@ -0,0 +1,48 @@
+#' Creates Parameter Vector
+#' 
+#' Creates a vector with assigned parameters of the given \code{\link{ODEnetwork}}.
+#' ACHTUNG: Soll nicht exportiert werden
+#'
+#' @param odenetwork [\code{ODEnetwork}]\cr
+#'   List of class \code{\link{ODEnetwork}}.
+#' @return a named vector with assigned values
+#' @examples
+#' createParamVec(odenetwork)
+createParamVec <- function(odenet) {
+  UseMethod("createParamVec")
+}
+createParamVec.ODEnetwork <- function(odenet) {
+  # test arguments
+  checkArg(odenet, "ODEnetwork", na.ok=FALSE)
+  
+  # Parametervektor starten
+  strParams <- "c("
+  # Anregungen, Massen, Daempfer- und Federkonstanten auflisten	
+  for (i in 1:length(odenet$masses)) {
+    # Masse
+    strParams <- paste(strParams, "m.", i, " = ", odenet$masses[i], ", ", sep = "")
+    # Daempfer
+    strParams <- paste(strParams, "d.", i, " = ", diag(odenet$dampers)[i], ", ", sep = "")
+    # Feder
+    strParams <- paste(strParams, "k.", i, " = ", diag(odenet$springs)[i], sep = "")
+    # Daempfer und Feder zu den verknuepften Massen
+    for (j in 1:length(odenet$masses)) {
+      # Aktuellen Knoten ueberspringen
+      if (j == i)
+        next
+      if (odenet$dampers[i, j] != 0) {
+        strParams <- paste(strParams, ", d.", i, ".", j, " = ", odenet$dampers[i, j], ", ", sep = "")
+      }
+      if (odenet$springs[i, j] != 0) {
+        strParams <- paste(strParams, "k.", i, ".", j, " = ", odenet$springs[i, j], sep = "")
+      }
+    }
+    # Komma oder Abschluss
+    if (i < length(odenet$masses))
+      strParams <- paste(strParams, ",")
+    else
+      strParams <- paste(strParams, ")", sep = "")
+  }	
+  # Rueckgabe
+  return(eval(parse(text = strParams)))
+}
---
+++ skel/inst/tests/test_createParamVec.R
@@ -0,0 +1,23 @@
+context("Test creating parameter vector")
+
+test_that("setState", {
+  masses <- 1
+  dampers <- as.matrix(0.1)
+  springs <- as.matrix(4)
+  odenet <- ODEnetwork(masses, dampers, springs)
+  cPar <- createParamVec(odenet)
+  
+  expect_is(cPar, c("numeric", "vector"))
+  expect_equal(cPar, c(m.1=1, d.1=0.1, k.1=4))
+  
+  masses <- c(1, 2)
+  dampers <- diag(c(0.1, 0.5))
+  dampers[1, 2] <- 0.05
+  springs <- diag(c(4, 10))
+  springs[1, 2] <- 6
+  odenet <- ODEnetwork(masses, dampers, springs)
+  cPar <- createParamVec(odenet)
+  
+  expect_equal(cPar, c(m.1=1, d.1=0.1, k.1=4, d.1.2=0.05, k.1.2=6,
+                       m.2=2, d.2=0.5, k.2=10, d.2.1=0.05, k.2.1=6))
+})
