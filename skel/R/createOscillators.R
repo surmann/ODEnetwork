1f9ee70a3a51dbd8f59f6bcbe774861c80a6a886
Dirk Surmann <surmann@statistik.tu-dortmund.de>
2013-04-26 16:51:53 +0200

    current work on fixed states

--- skel/R/createOscillators.R
+++ skel/R/createOscillators.R
@@ -24,19 +24,19 @@ createOscillators.ODEnetwork <- function(odenet) {
   # Quelltext erstellen
   strFunktion <- "with(as.list(c(cState, cParameters)), {"
   # Testausgabe
-#   strTemp <- "print(c(unname(cState), unname(createState(odenet))))"
-#   strTemp <- "x.1 <- cStateNew[1]"
-#   strFunktion <- c(strFunktion, strTemp)
-#   strTemp <- "v.1 <- cStateNew[2]"
-#   strFunktion <- c(strFunktion, strTemp)
-#   strTemp <- "print(unname(c(x.1, cStateNew[1], v.1, cStateNew[2])))"
-#   strFunktion <- c(strFunktion, strTemp)
-#   strTemp <- "if (cTime > 0) print(c(cTime, lagvalue(cTime)))"
-#   strFunktion <- c(strFunktion, strTemp)
+  #   strTemp <- "print(c(unname(cState), unname(createState(odenet))))"
+  #   strTemp <- "x.1 <- cStateNew[1]"
+  #   strFunktion <- c(strFunktion, strTemp)
+  #   strTemp <- "v.1 <- cStateNew[2]"
+  #   strFunktion <- c(strFunktion, strTemp)
+  #   strTemp <- "print(unname(c(x.1, cStateNew[1], v.1, cStateNew[2])))"
+  #   strFunktion <- c(strFunktion, strTemp)
+  #   strTemp <- "if (cTime > 0) print(c(cTime, lagvalue(cTime)))"
+  #   strFunktion <- c(strFunktion, strTemp)
   strTemp <- bquote(currState <- createState(odenet, cTime))
   strFunktion <- c(strFunktion, strTemp)
-  strTemp <- bquote(print(unname(c(cTime, currState))))
-  strFunktion <- c(strFunktion, strTemp)
+  #   strTemp <- bquote(print(unname(c(cTime, currState))))
+  #   strFunktion <- c(strFunktion, strTemp)
   
   # Einzelnen Knoten durchgehen und die Differentialgleichungen erstellen
   for (i in 1:length(odenet$masses)) {
@@ -45,39 +45,53 @@ createOscillators.ODEnetwork <- function(odenet) {
     # Es werden nur Differentialgleichungen der 1. Ordnung verwendet
     # Test
     # dx <- v
-    if (i == 0)
-      strTemp <- paste("dx.", i, " <- 0", sep = "")
-    else
-      strTemp <- paste("dx.", i, " <- v.", i, sep = "")
+    strTemp <- paste("if (is.na(currState['x.", i, "'])) {", sep = "")
+    strFunktion <- c(strFunktion, strTemp)
+    strTemp <- paste("dx.", i, " <- v.", i, sep = "")
+    strFunktion <- c(strFunktion, strTemp)
+    strFunktion <- c(strFunktion, "} else if (cTime > 0.1) {")
+    strTemp <- paste("dx.", i, " <- (currState['x.", i, "'] - lagvalue(cTime-timestep(), ", 2*i-1, ")) / (cTime - timestep())", sep ="")
+    strFunktion <- c(strFunktion, strTemp)
+    strFunktion <- c(strFunktion, "} else {")
+    strTemp <- paste("dx.", i, " <- 0", sep = "")
+    strFunktion <- c(strFunktion, strTemp)
+    strFunktion <- c(strFunktion, "}")
+    # Testausgabe
+    strTemp <- bquote(print(unname(c(cTime, dx.1))))
     strFunktion <- c(strFunktion, strTemp)
     # dv1 <- (F1 - d*v1 - k*x1 - d12*(v1-v2) - k12*(x1-x2)) / m1
-    if (i == 0)
-      strTemp <- paste("dv.", i, " <- 0", sep = "")
-    else {
-      strTemp <- paste("dv.", i, " <- (", sep = "")
-      # Dämpfer der aktuellen Masse hinzufügen
-      if (odenet$dampers[i, i] != 0)
-        strTemp <- paste(strTemp, " - d.", i, "*v.", i, sep = "")
-      # Feder der aktuellen Masse hinzufügen
-      if (odenet$springs[i, i] != 0)
-        strTemp <- paste(strTemp, " - k.", i, "*x.", i, sep = "")
-      # Feder und Daempfer aller Koppelelemente
-      for (j in 1:length(odenet$masses)) {
-        # aktuellen Knoten ueberspringen
-        if (j == i)
-          next
-        # Daempfer und Feder, falls vorhanden
-        if (odenet$dampers[i, j] != 0) {
-          strTemp <- paste(strTemp, " - d.", i, ".", j, "*(v.", i, "-v.", j, ")", sep = "")
-        }
-        if (odenet$springs[i, j] != 0) {
-          strTemp <- paste(strTemp, " - k.", i, ".", j, "*(x.", i, "-x.", j, ")", sep = "")
-        }
-      }
-      # Abschluss: Klammer schließen und durch Masse teilen
-      strTemp <- paste(strTemp, ")/m.", i, sep = "")
-    }
+    strTemp <- paste("if (is.na(currState['v.", i, "'])) {", sep = "")
     strFunktion <- c(strFunktion, strTemp)
+    #     if (is.na(v.1)) {
+    strTemp <- paste("dv.", i, " <- (", sep = "")
+    # Dämpfer der aktuellen Masse hinzufügen
+    if (odenet$dampers[i, i] != 0)
+      strTemp <- paste(strTemp, " - d.", i, "*v.", i, sep = "")
+    # Feder der aktuellen Masse hinzufügen
+    if (odenet$springs[i, i] != 0)
+      strTemp <- paste(strTemp, " - k.", i, "*x.", i, sep = "")
+    # Feder und Daempfer aller Koppelelemente
+    for (j in 1:length(odenet$masses)) {
+      # aktuellen Knoten ueberspringen
+      if (j == i)
+        next
+      # Daempfer und Feder, falls vorhanden
+      if (odenet$dampers[i, j] != 0) {
+        strTemp <- paste(strTemp, " - d.", i, ".", j, "*(v.", i, "-v.", j, ")", sep = "")
+      }
+      if (odenet$springs[i, j] != 0) {
+        strTemp <- paste(strTemp, " - k.", i, ".", j, "*(x.", i, "-x.", j, ")", sep = "")
+      }
+    }
+    # Abschluss: Klammer schließen und durch Masse teilen
+    strTemp <- paste(strTemp, ")/m.", i, sep = "")
+    strFunktion <- c(strFunktion, strTemp)
+    strFunktion <- c(strFunktion, "} else {")
+    #     } else {
+    strTemp <- paste("dv.", i, " <- 0", sep = "")
+    strFunktion <- c(strFunktion, strTemp)
+    strFunktion <- c(strFunktion, "}")
+    #     }
   }
   
   # Rueckgabeliste der Werte des Zustandsraumes
